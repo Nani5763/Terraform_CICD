6bd56f2a0917fbd57e30c2bd9925f8368428202d
Pavan sai <pavanaws57@gmail.com>
2024-08-13 15:06:15 +0530

    update

--- Main.tf
+++ Main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 resource "aws_instance" "pavan" {
     ami = "ami-0a4408457f9a03be3"
-    instance_type = "t2.micro"
+    instance_type = "t2.medium"
     key_name = "Ansible"
     tags = {
       Name = "Pavan-dev"
