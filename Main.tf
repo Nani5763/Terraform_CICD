44afe38fd32febc4618ba478764d0b6a629d8ccb
Pavan sai <pavanaws57@gmail.com>
2024-08-14 09:26:48 +0530

    updates

--- Main.tf
+++ Main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 resource "aws_instance" "pavan" {
     ami = "ami-0ae8f15ae66fe8cda"
-    instance_type = "t2.micro"
+    instance_type = "t2.nano"
     key_name = "Kubernetes"
     tags = {
       Name = "Pavan-dev"
